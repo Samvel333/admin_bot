0fc3f293c213073590708c88c6273277b9bce908
Samvel333 <sazdav601@gmail.com>
2022-04-06 16:45:40 +0400

    d

---
+++ index.js
@@ -0,0 +1,10 @@
+const { Telegraf } = require('telegraf')
+require('dotenv').config()
+
+const bot = new Telegraf(process.env.TOKEN)
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
+console.log(bot)
